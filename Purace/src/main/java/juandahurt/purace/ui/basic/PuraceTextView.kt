fec0fd924e775b29fb4a528cf251ddc30b08c46c
Juan David Hurtado <juandahurtadom@gmail.com>
2022-10-03 20:45:35 -0500

    initial: project setup

---
+++ Purace/src/main/java/juandahurt/purace/ui/basic/PuraceTextView.kt
@@ -0,0 +1,9 @@
+package juandahurt.purace.ui.basic
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun PuraceTextView() {
+    Text(text = "Example")
+}
---
+++ app/src/main/java/juandahurt/puracedemo/MainActivity.kt
@@ -0,0 +1,23 @@
+package juandahurt.puracedemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import juandahurt.purace.ui.basic.PuraceTextView
+import juandahurt.puracedemo.ui.theme.PuraceDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PuraceTextView()
+        }
+    }
+}
